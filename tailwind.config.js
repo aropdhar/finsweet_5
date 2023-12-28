d0d176446bc0a40ce99d1bc25710fcbc27440c65
aropdhar <142885013+aropdhar@users.noreply.github.com>
2023-12-28 14:16:21 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          navbarbg: '#0D0D0D',
+          color: '#fff',
+          primary: '#FFD2A4',
+          black: '#000',
+
+
+
+        },
+        maxWidth: {
+            'container': '1280px',
+        },
+
+        fontFamily: {
+            'roboto': " 'Roboto Condensed', sans-serif ",
+            
+        },
+
+        backgroundImage: {
+            'bannerbg': "url('./images/image.png')",
+            
+          },
+      }
+    }
+  }
